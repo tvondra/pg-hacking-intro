22d7e52bf2650268c782beb0e7f2093b96b5e338
Tomas Vondra <tomas@vondra.me>
2025-01-27 15:29:08 +0100

    add extension for exercise #11

---
+++ 10-adding-guc-parameters/extension/hacking.c
@@ -0,0 +1,118 @@
+/*
+ * hacking.c - a skeleton of a minimal extension
+ */
+
+#include "postgres.h"
+#include "fmgr.h"
+#include "utils/builtins.h"
+#include "utils/guc.h"
+#include "utils/memutils.h"
+#include "varatt.h"
+
+PG_MODULE_MAGIC;
+
+/* example function */
+PG_FUNCTION_INFO_V1(hacking_function);
+PG_FUNCTION_INFO_V1(hacking_function_2);
+PG_FUNCTION_INFO_V1(hacking_function_3);
+
+static bool my_dev_flag = false;
+
+void
+_PG_init(void)
+{
+	/* Define custom GUC variables. */
+	DefineCustomBoolVariable("hacking.development_flag",
+							 "custom flag",
+							 NULL,
+							 &my_dev_flag,
+							 false,
+							 PGC_USERSET,
+							 GUC_NOT_IN_SAMPLE,
+							 NULL,
+							 NULL,
+							 NULL);
+
+	MarkGUCPrefixReserved("hacking");
+}
+
+Datum
+hacking_function(PG_FUNCTION_ARGS)
+{
+	MemoryContext old_ctx, ctx;
+	char	*tmp;
+
+	ctx =  AllocSetContextCreate(CurrentMemoryContext,
+                                "hacking function context",
+                                ALLOCSET_DEFAULT_SIZES);
+
+	old_ctx = MemoryContextSwitchTo(ctx);
+
+	tmp = (char *) palloc(8192*10);
+	pfree(tmp);
+
+	MemoryContextStats(ctx);
+	MemoryContextSwitchTo(old_ctx);
+	MemoryContextDelete(ctx);
+
+	PG_RETURN_VOID();
+}
+
+Datum
+hacking_function_2(PG_FUNCTION_ARGS)
+{
+	int32	value = PG_GETARG_INT32(0);
+
+	elog(WARNING, "value = %d", value);
+
+	PG_RETURN_VOID();
+}
+
+Datum
+hacking_function_3(PG_FUNCTION_ARGS)
+{
+	text	*msg;
+	char	*msg_str;
+	int		msg_size;
+	char   *ret;
+
+	MemoryContext oldctx;
+
+	/* create a local context for our temporary stuff */
+	MemoryContext ctx = AllocSetContextCreate(CurrentMemoryContext,
+											  "my test context",
+											  ALLOCSET_DEFAULT_SIZES);
+
+	/* switch to the new local context */
+	oldctx = MemoryContextSwitchTo(ctx);
+
+	msg = (text *) PG_DETOAST_DATUM(PG_GETARG_DATUM(0));
+	msg_str = VARDATA_ANY(msg);
+	msg_size = VARSIZE_ANY_EXHDR(msg);
+
+	for (int i = 0; i < 100; i++)
+	{
+		char   *ptr = palloc(1328);
+		pfree(ptr);
+	}
+
+	/* switch "back" to the original context */
+	MemoryContextSwitchTo(oldctx);
+
+	/* print top-level context stats, then stats for our context */
+	MemoryContextStats(TopMemoryContext);
+	MemoryContextStats(ctx);
+
+	elog(WARNING, "message = %s, size = %d", msg_str, msg_size);
+
+	/* have to do the conversion before destroying the source */
+	ret = text_to_cstring(msg);
+
+	/* delete the next context */
+	MemoryContextDelete(ctx);
+
+	/* print the top-level stats again */
+	MemoryContextStats(TopMemoryContext);
+
+	PG_RETURN_CSTRING(ret);
+}
